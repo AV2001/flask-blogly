900bd00825c731c9b15024b8f3ee320c2e4bacda
AV2001 <anirudhvadlamani2001@gmail.com>
2023-08-21 02:17:06 +0400

    enable flask debug toolbar, setup database connectivity, and add root route

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'nfufbo2fif42'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+# Initialize flask app to use the database
+db.init_app(app)
+db.create_all()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
