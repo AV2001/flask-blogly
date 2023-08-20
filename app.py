9abd6f7569f146943c207d05499546039142b126
AV2001 <anirudhvadlamani2001@gmail.com>
2023-08-21 01:49:52 +0400

    initial commit

---
+++ app.py
@@ -0,0 +1,6 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
