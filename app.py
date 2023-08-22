05327ddca7bc8cfeb104f8001e6a700a46b15ad3
AV2001 <anirudhvadlamani2001@gmail.com>
2023-08-22 04:12:31 +0400

    add routes to display all users and render a form for adding new user

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, User
 
@@ -12,9 +12,24 @@ debug = DebugToolbarExtension(app)
 
 # Initialize flask app to use the database
 db.init_app(app)
-db.create_all()
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
 def index():
+    '''Show the home page.'''
     return render_template('index.html')
+
+
+@app.route('/users')
+def get_all_users():
+    '''Show all the users.'''
+    users = User.query.all()
+    return render_template('users.html', users=users)
+
+
+@app.route('/users/new')
+def add_new_user():
+    '''Render a template containing a form for adding a user.'''
+    return render_template('add-user.html')
