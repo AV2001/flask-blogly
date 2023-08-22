7381beaacd43aa02cf7c43f3230256a4094409b1
AV2001 <anirudhvadlamani2001@gmail.com>
2023-08-22 04:15:26 +0400

    overwrite the repr method to represent the created user

--- models.py
+++ models.py
@@ -9,4 +9,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
-    image_url = db.Column(db.String)
+    image_url = db.Column(
+        db.String, default='https://i.stack.imgur.com/l60Hf.png')
+
+    def __repr__(self):
+        return f'<User id={self.id} first_name={self.first_name} last_name={self.last_name} image_url={self.image_url}>'
