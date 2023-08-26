07f3391dc8ad42008ecd3680c65e7a9ae0c425fc
AV2001 <anirudhvadlamani2001@gmail.com>
2023-08-26 08:43:50 +0400

    set SQLALCHEMY_ECHO to False

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from models import db, User
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_ECHO'] = False
 app.config['SECRET_KEY'] = 'nfufbo2fif42'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
