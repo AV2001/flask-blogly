de32b7d9a1ede078425c01c0c220df57f069c609
AV2001 <anirudhvadlamani2001@gmail.com>
2023-08-28 19:35:42 +0400

    update seed file to populate posts

--- seed.py
+++ seed.py
@@ -1,13 +1,10 @@
-from models import db, User
+from models import db, User, Post
 from app import app
 
+# Create all tables
 with app.app_context():
-    # Create tables
     db.drop_all()
     db.create_all()
-
-    # Empty table if it isn't already empty.
-    User.query.delete()
 
 
 # Add users
@@ -15,9 +12,13 @@ john = User(first_name='John', last_name='Doe', image_url=None)
 jane = User(first_name='Jane', last_name='Doe', image_url=None)
 jill = User(first_name='Jill', last_name='Doe', image_url=None)
 
-users = [john, jane, jill]
+post1 = Post(title='First Post', user_id=1)
+post2 = Post(title='Second Post', user_id=1)
+post3 = Post(title='Third Post', user_id=1)
+
+data = [john, jane, jill, post1, post2, post3]
 
 with app.app_context():
     # Add all objects to the session
-    db.session.add_all(users)
+    db.session.add_all(data)
     db.session.commit()
