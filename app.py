dbe9a564418eabb7defc28dceedc66cb243adcb6
AV2001 <anirudhvadlamani2001@gmail.com>
2023-08-30 09:47:28 +0400

    add route to process form data to edit the post in the database

--- app.py
+++ app.py
@@ -120,7 +120,20 @@ def show_post(post_id):
 
 
 @app.route('/posts/<int:post_id>/edit')
-def edit_post(post_id):
+def show_edit_post_form(post_id):
     '''Render template containing a form to edit a post.'''
     post = Post.query.get(post_id)
     return render_template('edit-post.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit', methods=['POST'])
+def edit_form(post_id):
+    '''Process form data to edit a post.'''
+    title = request.form['title']
+    content = request.form['content']
+    post = Post.query.get(post_id)
+    post.title = title
+    post.content = content
+    db.session.add(post)
+    db.session.commit()
+    return redirect('/users')
