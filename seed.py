f3c308a390b9de2c492db5ccbd4178e869f8d8eb
AV2001 <anirudhvadlamani2001@gmail.com>
2023-08-31 11:22:17 +0400

    call get_current_date_time from utilities

--- seed.py
+++ seed.py
@@ -1,5 +1,6 @@
 from models import db, User, Post
 from app import app
+from utilities import get_current_date_time
 
 # Create all tables
 with app.app_context():
@@ -12,9 +13,10 @@ john = User(first_name='John', last_name='Doe', image_url=None)
 jane = User(first_name='Jane', last_name='Doe', image_url=None)
 jill = User(first_name='Jill', last_name='Doe', image_url=None)
 
-post1 = Post(title='First Post', user_id=1)
-post2 = Post(title='Second Post', user_id=1)
-post3 = Post(title='Third Post', user_id=1)
+post1 = Post(title='First Post', user_id=1, created_at=get_current_date_time())
+post2 = Post(title='Second Post', user_id=1,
+             created_at=get_current_date_time())
+post3 = Post(title='Third Post', user_id=1, created_at=get_current_date_time())
 
 data = [john, jane, jill, post1, post2, post3]
 
