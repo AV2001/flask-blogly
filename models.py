abf796017a5c5280ebe3b908bf01188c13bc3606
AV2001 <anirudhvadlamani2001@gmail.com>
2023-08-31 12:05:30 +0400

    change data type in Post model created_at from db.String to db.DateTime

--- models.py
+++ models.py
@@ -28,8 +28,9 @@ class Post(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.Text, nullable=False)
-    content = db.Column(db.Text)
-    created_at = db.Column(db.String)
+    content = db.Column(db.Text, nullable=False,
+                        default='This is a post with no content.')
+    created_at = db.Column(db.DateTime)
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     user = db.relationship('User', backref='posts', lazy='joined')
